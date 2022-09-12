882205b41a2bb5c333d481a9d2928506416fe0f1
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-09-12 22:31:27 +0200

    Add 02-15

---
+++ src/02-basic-types/15-typescript-type-inference.solution.ts
@@ -0,0 +1,31 @@
+import { expect, it } from "vitest";
+
+// Task: Merge declaration and initialization of these variables to utilize type inference
+const name = "Drizzt";
+const level = 20;
+
+it("Should treat name as a string", () => {
+  expect(name.substring(2)).toBe("izzt");
+});
+it("Should treat level as a number", () => {
+  expect(level.toFixed(1)).toBe("20.0");
+});
+
+// Task: Add default values that help infer the type of the argument
+const getData = (
+  url = "/",
+  method = "GET",
+  pagination = { index: 0, limit: 20 }
+) => {
+  console.log(`[${method}] ${url} (${pagination.index},${pagination.limit})`);
+
+  return [123, 333, 555];
+};
+
+it("Should return the array of data from the getData function", () => {
+  expect(getData("/", "POST", { index: 0, limit: 3 })).toStrictEqual([
+    123, 333, 555,
+  ]);
+});
+
+const getPostNumberRegEx = () => /^\d{4}$/;
