e06c7c06a546eb512e3873791842a3a494a60502
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-09-12 20:37:18 +0200

    Add 02-02

---
+++ src/02-basic-types/02-typescript-number.problem.ts
@@ -0,0 +1,10 @@
+let lineCount: never;
+let radius: never;
+let size: never;
+
+lineCount = 9;
+radius = 3.14;
+size = 9999999999999999999n;
+
+// Needed to make this a module
+export {};
---
+++ src/02-basic-types/02-typescript-number.solution.ts
@@ -0,0 +1,10 @@
+let lineCount: number;
+let radius: number;
+let size: bigint;
+
+lineCount = 9;
+radius = 3.14;
+size = 9999999999999999999n;
+
+// Needed to make this a module
+export {};
