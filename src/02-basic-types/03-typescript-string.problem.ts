1541c87843a99d12e71e1bae56f78bacba63be60
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-09-12 20:39:38 +0200

    Add 02-03

---
+++ src/02-basic-types/03-typescript-string.problem.ts
@@ -0,0 +1,14 @@
+let firstName: never;
+let lastName: never;
+let bio: never;
+
+firstName = "Kaladin";
+lastName = 'Stormblessed';
+bio = `\
+In the storm he was born...
+
+And since blessed.\
+`;
+
+// Needed to make this a module
+export {};
---
+++ src/02-basic-types/03-typescript-string.solution.ts
@@ -0,0 +1,14 @@
+let firstName: string;
+let lastName: string;
+let bio: string;
+
+firstName = "Kaladin";
+lastName = 'Stormblessed';
+bio = `\
+In the storm he was born...
+
+And since blessed.\
+`;
+
+// Needed to make this a module
+export {};
