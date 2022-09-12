bf2505e0eebf8be7592fe25d4ed4aae214b27dc5
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-09-12 22:17:52 +0200

    Add 02-14

---
+++ src/02-basic-types/14-typescript-string-literal-types.problem.ts
@@ -0,0 +1,14 @@
+type ValidStatus = "loading" | "done" | "failed";
+
+let noGiveOnlyPlay: "play";
+let friends: ["Ross", "Monica", "Joey", "Chandler", "Phoebe", "Rachel"];
+
+noGiveOnlyPlay = "give";
+friends = ["Tinky Winky", "Dipsy", "Laa-Laa", "Po"];
+
+const status1: ValidStatus = "what";
+const status2: ValidStatus = "what";
+const status3: ValidStatus = "what";
+
+// Needed to make this a module
+export {};
---
+++ src/02-basic-types/14-typescript-string-literal-types.solution.ts
@@ -0,0 +1,14 @@
+type ValidStatus = "loading" | "done" | "failed";
+
+let noGiveOnlyPlay: "play";
+let friends: ["Ross", "Monica", "Joey", "Chandler", "Phoebe", "Rachel"];
+
+noGiveOnlyPlay = "play";
+friends = ["Ross", "Monica", "Joey", "Chandler", "Phoebe", "Rachel"];
+
+const status1: ValidStatus = "loading";
+const status2: ValidStatus = "done";
+const status3: ValidStatus = "failed";
+
+// Needed to make this a module
+export {};
