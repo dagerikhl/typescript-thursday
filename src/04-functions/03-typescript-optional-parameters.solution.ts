205e13835bd20a122be3b58f6d21151913339465
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-09-13 08:11:54 +0200

    Add 04-03

---
+++ src/04-functions/03-typescript-optional-parameters.solution.ts
@@ -0,0 +1,50 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+// Task: Add types to this function, make some optional to satisfy tests
+const log = (
+  message: string,
+  includeTimestamp?: boolean,
+  indent?: number
+): void => {
+  let output = message;
+
+  if (includeTimestamp) {
+    output = `[${Date.now()}] ${output}`;
+  }
+
+  if (indent) {
+    output = `${"  ".repeat(indent)}${output}`;
+  }
+
+  console.log(output);
+};
+
+describe("typescript-optional-parameters", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("Should log a message", () => {
+    const spy = vi.spyOn(console, "log");
+
+    log("Request sent");
+
+    expect(spy).toHaveBeenNthCalledWith(1, "Request sent");
+  });
+
+  it("Should log a message with a timestamp", () => {
+    const spy = vi.spyOn(console, "log");
+
+    log("Request sent", true);
+
+    expect(spy.mock.calls[0][0]).toMatch(/^\[\d{13}\] Request sent$/);
+  });
+
+  it("Should log an indented message", () => {
+    const spy = vi.spyOn(console, "log");
+
+    log("Request sent", false, 3);
+
+    expect(spy.mock.calls[0][0]).toBe("      Request sent");
+  });
+});
