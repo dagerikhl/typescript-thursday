1e59c6d2929208c4fcd7a3320959407086a3a56a
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-09-13 21:27:14 +0200

    Add 07-02

---
+++ src/07-advanced-types/02-typescript-type-guards.solution.ts
@@ -0,0 +1,82 @@
+import { expect, it } from "vitest";
+
+class Coordinates {
+  public x: number;
+  public y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+interface Response {
+  data: any[];
+  total: number;
+}
+
+// Task: Impl. your own type guard asserting that the passed parameter is an object that satisfies the Response interface
+const isResponse = (value: any): value is Response => {
+  const hasTwoProperties = Object.keys(value).length === 2;
+  const hasCorrectDataProperty = "data" in value && Array.isArray(value.data);
+  const hasCorrectTotalPropert =
+    "total" in value && typeof value.total === "number";
+
+  return hasTwoProperties && hasCorrectDataProperty && hasCorrectTotalPropert;
+};
+
+const formatEntity = (
+  entity: boolean | number | Coordinates | unknown
+): string => {
+  // Task: Impl. this if condition using a typeof type guard
+  if (typeof entity === "boolean") {
+    return entity ? "YES" : "NO";
+  }
+
+  // Task: Impl. this if condition type guard
+  if (typeof entity === "number") {
+    return entity.toFixed(2);
+  }
+
+  // Task: Impl. this if condition with a type guard for the Coordinates class
+  if (entity instanceof Coordinates) {
+    return `${entity.x}x${entity.y}`;
+  }
+
+  if (isResponse(entity)) {
+    return `[TOTAL ITEMS: ${entity.total}]: DATA: ${entity.data.join(", ")}`;
+  }
+
+  return JSON.stringify(entity);
+};
+
+it("Should format a boolean correctly", () => {
+  expect(formatEntity(true)).toBe("YES");
+});
+
+it("Should format a number correctly", () => {
+  expect(formatEntity(16)).toBe("16.00");
+});
+
+it("Should format a Coordinates class correctly", () => {
+  const coordinates = new Coordinates(71, 660);
+
+  expect(formatEntity(coordinates)).toBe("71x660");
+});
+
+it("Should format a Response correctly", () => {
+  const response: Response = {
+    data: ["Soul Stone", "Carsomyr"],
+    total: 2,
+  };
+
+  expect(formatEntity(response)).toBe(
+    "[TOTAL ITEMS: 2]: DATA: Soul Stone, Carsomyr"
+  );
+});
+
+it("Should stringify something unknown in JSON format", () => {
+  const value = [true, { name: "Shallan" }, 560];
+
+  expect(formatEntity(value)).toBe('[true,{"name":"Shallan"},560]');
+});
