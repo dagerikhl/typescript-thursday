fa5e1479baf13b16a9e4827b64582d177619c7a9
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-09-13 12:18:29 +0200

    Add 06-01

---
+++ src/06-interfaces/01-typescript-interface.problem.ts
@@ -0,0 +1,41 @@
+import { expect, it } from "vitest";
+
+// Task: Rewrite these type aliases to interfaces
+// Note: All tests pass for this exercise, but as long as there are `type` in the file, we're not done
+type ResponseHeaders = {
+  [key: string]: string;
+};
+type Response = {
+  data: any[];
+  total: number;
+  responseHeaders?: ResponseHeaders;
+};
+type UseFetch = {
+  isLoading?: boolean;
+  error?: Error;
+  response?: Response;
+};
+
+const useFetch = (): UseFetch => {
+  return {
+    isLoading: false,
+    response: {
+      data: [123, 555],
+      total: 2,
+      responseHeaders: { "Content-Type": "application/json" },
+    },
+  };
+};
+
+it("Should fetch a response, assuming the API responds", () => {
+  const fetchApi = useFetch();
+
+  expect(fetchApi).toStrictEqual({
+    isLoading: false,
+    response: {
+      data: [123, 555],
+      total: 2,
+      responseHeaders: { "Content-Type": "application/json" },
+    },
+  });
+});
